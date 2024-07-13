7f961531b43175b1424fe28039b8e5df6fc567a8
devhong <popodaddy@gmail.com>
2024-07-13 23:55:14 +0900

    API 서버를 위한 JWT, CORS, DB 등 기본작업

---
+++ src/main/java/com/raonworks/boardback/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.raonworks.boardback.filter;
+
+import com.raonworks.boardback.provider.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  private final JwtProvider jwtProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    try {
+      String token = parseBearerToken(request);
+      if (token == null) {
+        //pass to next filter.
+        filterChain.doFilter(request, response);
+        return;
+      }
+
+      String email = jwtProvider.validate(token);
+      if (email != null) {
+        //pass to next filter.(include expired date)
+        filterChain.doFilter(request, response);
+        return;
+      }
+
+      AbstractAuthenticationToken authenticationToken =
+              new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+
+      authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+      SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+      securityContext.setAuthentication(authenticationToken);
+      SecurityContextHolder.setContext(securityContext);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String parseBearerToken(HttpServletRequest request) {
+    String authorizationHeader = request.getHeader("Authorization");
+    boolean hasAuthorization = authorizationHeader != null && authorizationHeader.startsWith("Bearer ");
+    if(!hasAuthorization) return null;
+
+    return authorizationHeader.substring(7);
+  }
+}
---
+++ src/main/java/com/raonworks/boardback/provider/JwtProvider.java
@@ -0,0 +1,43 @@
+package com.raonworks.boardback.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+  @Value("${secret-key}")
+  private String secretKey;
+
+  public String create(String email) {
+    Date expireDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+    return Jwts.builder()
+            .signWith(SignatureAlgorithm.ES256, secretKey)
+            .setSubject(email)
+            .setIssuedAt(new Date())
+            .setExpiration(expireDate)
+            .compact();
+  }
+
+  public String validate(String jwt) {
+    Claims claims = null;
+    try {
+      claims = Jwts.parser()
+              .setSigningKey(secretKey)
+              .parseClaimsJws(jwt)
+              .getBody();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+    return claims.getSubject();
+  }
+}
